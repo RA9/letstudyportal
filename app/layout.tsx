4750983f4b2f396766fa6ee5c47ab6d640c3cdc0
Carlos Nah <cnah27@gmail.com>
2024-11-10 13:38:33 +0000

    update nav item with nav link url

--- app/layout.tsx
+++ app/layout.tsx
@@ -275,7 +275,7 @@ export default function RootLayout({
                   <NavigationMenuList>
                     <NavigationMenuItem>
                       <Link
-                        href="#"
+                        href="/about-us"
                         className="w-max rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
                       >
                         About Us
