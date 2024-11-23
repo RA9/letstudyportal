976e58082d6c643a1826c171de966f3e9a6de1a1
Carlos Nah <cnah27@gmail.com>
2024-11-23 23:43:24 +0000

    feat(auth): implement session decryption and middleware for route protection

--- app/lib/session.ts
+++ app/lib/session.ts
@@ -1,5 +1,10 @@
 // import 'server-only'
 import { cookies } from 'next/headers'
+import { SignJWT, jwtVerify } from 'jose'
+
+
+const secretKey = process.env.SESSION_SECRET
+const encodedKey = new TextEncoder().encode(secretKey)
 
 export async function createSession(token: string) {
     //   const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
@@ -19,6 +24,17 @@ export async function createSession(token: string) {
 
     return null
 }
+
+export async function decrypt(session: string | undefined = '') {
+    try {
+      const { payload } = await jwtVerify(session, encodedKey, {
+        algorithms: ['HS256'],
+      })
+      return payload
+    } catch (error) {
+      console.log('Failed to verify session')
+    }
+  }
 
 
 export async function deleteSession() {
