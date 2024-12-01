6683e273f9b80a203b12d72e4bc40af23d3915b0
Carlos Nah <cnah27@gmail.com>
2024-12-01 06:54:51 +0000

    feat(manage-schools): enhance SchoolsPage layout with dashboard title and action buttons for adding and exporting schools

--- app/(loggedin)/manage-schools/page.tsx
+++ app/(loggedin)/manage-schools/page.tsx
@@ -1,5 +1,7 @@
 import { SchoolList } from "@/components/SchoolListing"
+import { Button } from "@/components/ui/button"
 import type { Metadata } from 'next'
+import Link from "next/link"
 
 export const metadata: Metadata = {
   title: 'Manage Schools - Let\'s Study Portal',
@@ -8,8 +10,18 @@ export const metadata: Metadata = {
 
 export default function SchoolsPage() {
   return (
-    <div className="container mx-auto py-10">
-      <h1 className="text-2xl font-bold mb-5">Schools Directory</h1>
+    <div className="container mx-auto py-10 px-4 bg-white">
+        <div className="flex items-center justify-between mb-5">
+        <h1 className="text-2xl font-bold">Dashboard</h1>
+        <div className="flex items-center gap-2">
+            <Link href="/manage-schools/create">
+                <Button variant="default">Add School</Button>
+            </Link>
+            <Link href="/manage-schools/create">
+                <Button variant="blue">Export</Button>
+            </Link>
+        </div>
+        </div>
       <SchoolList />
     </div>
   )
